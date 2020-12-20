b62cf59fd051cff6f11db02e3bab276f99eccb94
sooyoung ho <ghtndud1993@gmail.com>
2020-12-20 21:35:55 +0900

    새로운 매뉴얼 다운로드 추가

--- routes/main.js
+++ routes/main.js
@@ -6,10 +6,8 @@ var mime = require('mime');
 var getDownloadFilename = require('./lib/Filename').getDownloadFilename;
 router.post('/DownloadFile_1', function(req, res, next) {
     //var upload_folder = 'C:/test/';
-    var file = './downloads/fjbox_ui_manual.pdf';
-    console.log('1');
+    var file = './downloads/fjbox_growth_manual.pdf';
     try {
-        console.log('2');
 
         if (fs.existsSync(file)) { // 파일이 존재하는지 체크
             var filename = path.basename(file); // 파일 경로에서 파일명(확장자포함)만 추출
@@ -34,7 +32,7 @@ router.post('/DownloadFile_1', function(req, res, next) {
 });
 router.post('/DownloadFile_2', function(req, res, next) {
     //var upload_folder = 'C:/test/';
-    var file = 'C:/test/테스트2.pdf';
+    var file = './downloads/fjbox_ui_manual.pdf';
     console.log('11');
     try {
         console.log('22' + file);
@@ -64,7 +62,7 @@ router.post('/DownloadFile_2', function(req, res, next) {
 ////////////////////////////////////////////////////
 router.post('/DownloadFile_3', function(req, res, next) {
     //var upload_folder = 'C:/test/';
-    var file = 'C:/test/테스트2.pdf';
+    var file = './downloads/fjbox_nutrient_manual.pdf';
     console.log('11');
     try {
         console.log('22' + file);
@@ -92,7 +90,38 @@ router.post('/DownloadFile_3', function(req, res, next) {
     }
 });
 /////////////////////////////////////////////////
+////////////////////////////////////////////////////
 router.post('/DownloadFile_4', function(req, res, next) {
+    //var upload_folder = 'C:/test/';
+    var file = './downloads/fjbox_device_manual.pdf';
+    console.log('11');
+    try {
+        console.log('22' + file);
+        if (fs.existsSync(file)) { // 파일이 존재하는지 체크
+            var filename = path.basename(file); // 파일 경로에서 파일명(확장자포함)만 추출
+            var mimetype = mime.getType(file); // 파일의 타입(형식)을 가져옴
+            console.log('33');
+            console.log('filename: ' + filename);
+            console.log('mimetype: ' + mimetype);
+            console.log('req: ' + req);
+            res.setHeader('Content-disposition', 'attachment; filename=' + getDownloadFilename(req, filename)); // 다운받아질 파일명 설정
+            res.setHeader('Content-type', mimetype); // 파일 형식 지정
+            var filestream = fs.createReadStream(file);
+            filestream.pipe(res);
+        } else {
+            console.log('44');
+            res.send('해당 파일이 없습니다.');
+            return;
+        }
+    } catch (e) { // 에러 발생시
+        console.log('55');
+        console.log(e);
+        res.send('파일을 다운로드하는 중에 에러가 발생하였습니다.');
+        return;
+    }
+});
+/////////////////////////////////////////////////
+router.post('/DownloadFile_5', function(req, res, next) {
     //var upload_folder = 'C:/test/';
     var file = './downloads/info_law.pdf';
     console.log('11');
@@ -122,7 +151,7 @@ router.post('/DownloadFile_4', function(req, res, next) {
     }
 });
 ///////////////////////////////////////////////////
-router.post('/DownloadFile_5', function(req, res, next) {
+router.post('/DownloadFile_6', function(req, res, next) {
     //var upload_folder = 'C:/test/';
     var file = './downloads/homepage_law.pdf';
     console.log('11');
